c1c43c0266ad8f10f270e91533d9e6d94ee020d8
tanacchi <q111026d@mail.kyutech.jp>
2021-02-03 21:34:12 +0900

    Add form of new category

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [category_name, setCategoryName] = React.useState('');
+
+  const onTextAreaChange = (event) => {
+    setCategoryName(event.target.value);
+  }
+
+  const onSubmit = (event) => {
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +27,8 @@ function App() {
         >
           Learn React
         </a>
+        <textarea onChange={onTextAreaChange} />
+        <button onClick={onSubmit}>Add Category</button>
       </header>
     </div>
   );
