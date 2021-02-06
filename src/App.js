cca2512e0c5bb37ed65a0af4c5f8c01b7c71caea
tanacchi <q111026d@mail.kyutech.jp>
2021-02-07 01:01:44 +0900

    Remove unnecessary elements

--- src/App.js
+++ src/App.js
@@ -38,19 +38,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <textarea value={categoryName} onChange={onTextAreaChange} />
+        <input type="text" placeholder="New Category" value={categoryName} onChange={onTextAreaChange} />
         <button onClick={onSubmit} disabled={categoryName === ''}>Add Category</button>
         <CategorySelector />
       </header>
